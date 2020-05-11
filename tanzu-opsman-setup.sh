e113f3dde009db7c607aa803a9d202012b26b41a
thecloudgarage <39495790+thecloudgarage@users.noreply.github.com>
2020-05-11 12:37:41 +0530

    Update and rename setup1.sh to tanzu-opsman-setup.sh

--- tanzu-opsman-setup.sh
+++ tanzu-opsman-setup.sh
@@ -121,3 +121,17 @@ curl "https://localhost/api/v0/staged/products" -k \
   -H 'Authorization: Bearer '"$uaaToken"'' \
   -H "Content-Type: application/json" \
   -d '{"name": "harbor-container-registry", "product_version": "1.10.1-build.7"}'
+export uaaToken=$( sudo curl -s -k -H 'Accept: application/json;charset=utf-8' -d 'grant_type=password' -d 'username=admin' -d 'password=admin' \
+  -u 'opsman:' https://localhost/uaa/oauth/token |  jq --raw-output '.access_token' )
+export pasguid=$( curl "https://localhost/api/v0/staged/products" -k \
+  -X GET \
+  -H 'Authorization: Bearer '"$uaaToken"'' \
+  | jq --raw-output '.[] | select(.installation_name|test("cf-.")) | .guid')
+echo "PAS GU-ID is: $pasguid"
+curl "https://localhost/api/v0/staged/products/$pasguid/verifiers/install_time/WildcardDomainVerifier" -k \
+  -X PUT \
+  -H 'Authorization: Bearer '"$uaaToken"'' \
+  -H "Content-Type: application/json" \
+  -d '{ "enabled": false }'
+sudo rm -rf /home/ubuntu/token
+sudo rm -rf /home/ubuntu/dns
